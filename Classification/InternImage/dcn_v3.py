aaf1971cc70116f48ffaf9277c004d268dae8f25
anirudh6415 <anirudh.9591@gmail.com>
2024-02-09 15:30:06 -0700

    changes to dcnv3

--- Classification/InternImage/dcn_v3.py
+++ Classification/InternImage/dcn_v3.py
@@ -555,7 +555,7 @@ def create_dummy_func(func, dependency, message=""):
 
 
 # try:
-#     from DCNv3 import _C
+    import DCNv3 as _C
 # except ImportError:
 #     # TODO: register ops natively so there is no need to import _C.
 #     _msg = "detrex is not compiled successfully, please build following the instructions!"
